88957d8e9f5d5f595713fc206274beca79a8857e
suriani <tristano.suriani@ordina.nl>
2023-05-30 23:15:31 +0200

    added method to findById to AdministrationAgreementRepository

--- src/main/java/nl/suriani/dmexample/application/port/AdministrationAgreementRepository.java
+++ src/main/java/nl/suriani/dmexample/application/port/AdministrationAgreementRepository.java
@@ -2,6 +2,9 @@ package nl.suriani.dmexample.application.port;
 
 import nl.suriani.dmexample.domain.AdministrationAgreement;
 
+import java.util.Optional;
+
 public interface AdministrationAgreementRepository {
     void save(AdministrationAgreement administrationAgreement);
+    Optional<AdministrationAgreement> findById(String id);
 }
